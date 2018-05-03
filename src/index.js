8a3259d60096f3df594d5243a02bae62b3eb8edd
Bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-05-03 19:11:12 +0200

    Getting Started

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+
+import Game from './Game'
 
 ReactDOM.render(
-    <App />
-  , document.getElementById('root'))
+  <Game />
+  , document.getElementById('root')
+)
